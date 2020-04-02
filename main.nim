9cea3c32de96437a579a7f9434ad03dc9da700a7
Keith Devens <keith@keithdevens.com>
2020-04-01 21:14:45 -0600

    WIP: continue porting Python repo_status

--- main.nim
+++ main.nim
@@ -1,25 +1,35 @@
 import
+  algorithm,
+  os,
   osproc,
   parseopt,
   sequtils,
   strformat,
   strutils,
-  tables
+  tables,
+  terminal
 
 
 type
   StatusCode = enum
-    ahead,
-    behind,
-    staged,
-    added,
-    modified,
-    removed,
-    stashed,
-    untracked,
-    conflicted,
-    renamed,
+    ahead
+    behind
+    staged
+    added
+    modified
+    removed
+    stashed
+    untracked
+    conflicted
+    renamed
     unknown
+
+type
+  GitStatus = tuple
+    state: string
+    branch: string
+    status: Table[StatusCode, int]
+    stash: string
 
 
 proc parse(statusCode: string): StatusCode =
@@ -79,18 +89,137 @@ proc gitCmd(cmd: seq[string], workingdir: string): tuple[output: string,
   return execCmdEx cmd
 
 
-proc printRepoStatus(dir: string): int =
+proc getRepoState(git_dir_path: string): string =
+  ## Return a code for the current repo state.
+  ##
+  ## Possible states:
+  ##     R - rebase
+  ##     M - merge
+  ##     C - cherry-pick
+  ##     B - bisect
+  ##     V - revert
+  ##
+  ## The code returned will indicate multiple states (if that's possible?)
+  ## or the empty string if the repo is in a normal state.
+
+  # Unfortunately there's no porcelain to check for various git states.
+  # Determining state is done by checking for the existence of files within
+  # the git repository. Reference for possible checks:
+  # https://github.com/git/git/blob/master/contrib/completion/git-prompt.sh
+  let checks = {
+    "rebase-merge": 'R',
+    "rebase-apply": 'R',
+    "MERGE_HEAD": 'M',
+    "CHERRY_PICK_HEAD": 'C',
+    "BISECT_LOG": 'B',
+    "REVERT_HEAD": 'V',
+  }.toTable
+
+  var state_set: set[char]
+  for filename, status_code in checks:
+    let path = git_dir_path / filename
+    if path.fileExists:
+      state_set.incl status_code
+
+  return join(state_set.toSeq.sorted, "")
+
+
+proc writeStatusStr(status: GitStatus) =
+  # o, c = e[shell].o.replace('{', '{{'), e[shell].c.replace('}', '}}')
+  let format = [
+    (fgGreen, "↑", ahead),
+    (fgRed, "↓", behind),
+    (fgGreen, "●", staged),
+    (fgYellow, "+", modified),
+    (fgRed, "-", removed),
+    (fgCyan, "…", untracked),
+    (fgBlue, "⚑", stashed),
+    (fgRed, "✖", conflicted),
+  ]
+
+  # print state
+  stdout.styledWrite fgMagenta, status.state
+
+  # print branch
+  stdout.styledWrite fgYellow, status.branch
+
+  # print stats
+  for (color, token, code) in format:
+    let num = status.status.getOrDefault(code)
+    if num != 0:
+      stdout.styledWrite color, token, $num
+
+
+proc getRepoBranch(dir: string): string =
+  return "master"
+
+
+proc getStashCounts(dir: string): string =
+  return ""
+  # def get_stash_counts(repo):
+  #   stash_counts = Counter()
+
+  #   if repo.head_is_unborn:
+  #       return stash_counts  # can't stash on new repo
+
+  #   stashes = check_output(['git', 'stash', 'list'], cwd=repo.workdir).decode().splitlines()
+  #   getbranch = re.compile(r'^[^:]+:[^:]+?(\S+):')
+  #   for stash in stashes:
+  #       match = getbranch.match(stash)
+  #       if match:
+  #           # count the branch name
+  #           stash_counts.update([match[1]])
+  #       elif stash.split(':')[1].strip() == 'autostash':
+  #           # count autostash
+  #           stash_counts.update(['-autostash'])
+
+  #   return stash_counts
+
+
+proc getRepoStashes(dir: string): string =
+  ## Return a string like 1A for one stash on a branch and one autostash.
+  ##
+  ##  If a count is zero, indicate by leaving it out.
+  # counter = get_stash_counts(repo)
+  # if not counter:  # if no stashes, don't get stats
+  #     return ''
+  # else:
+  #     _total, branch_count, autostash = get_stash_stats(repo, counter)
+  #     branch_str = str(branch_count or '')
+  #     autostash_str = f"A{autostash if autostash > 1 else ''}" if autostash else ''
+  #     return f"{branch_str}{autostash_str}"
+  return ""
+
+
+proc getRepoStatus(dir: string): Table[StatusCode, int] =
   # get and parse status codes
   let cmd = @["status", "-z"]
   var (output, exitcode) = gitCmd(cmd, dir)
   let statusCodes = parseStatusCodes(output)
   var status = initTable[StatusCode, int]()
-  # ↑2 ↓2 ●2 +2 -2 ⚑2 …2 ✖2
 
+  # populate the status table
   for s in statusCodes:
     status.mgetOrPut(s, 0) += 1
 
-  echo status
+  # get stashes
+  status[stashed] = 1
+
+  return status
+
+
+proc printRepoStatus(dir: string): int =
+  # create the overall GitStatus
+  var gitstatus = (
+    state: getRepoState(dir),
+    branch: getRepoBranch(dir),
+    status: getRepoStatus(dir),
+    stash: getRepoStashes(dir),
+  )
+
+  # format the status codes into a string suitable for printing in the prompt
+  writeStatusStr(gitstatus)
+
   return 0
 
 
